f44b0d17f317410926a119af56b5b4a2250dc7c2
unknown <austinlee199723@yahoo.com>
2024-07-14 13:52:45 -0700

    initial

--- sanity.config.js
+++ sanity.config.js
@@ -1,18 +1,35 @@
+import {orderableDocumentListDeskItem} from '@sanity/orderable-document-list'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
+  //...
   name: 'default',
   title: 'alec profile',
 
   projectId: 'cuc1yznu',
   dataset: 'production',
+  plugins: [
+    structureTool({
+      structure: (S, context) => {
+        return S.list()
+          .title('Content')
+          .items([
+            // Minimum required configuration
+            orderableDocumentListDeskItem({type: 'reel', S, context}),
 
-  plugins: [structureTool(), visionTool()],
-
+            orderableDocumentListDeskItem({type: 'gallery', S, context}),
+            orderableDocumentListDeskItem({type: 'home', S, context}),
+            // orderableDocumentListDeskItem({type: 'contact', S, context}),
+            // ... all other desk items
+          ])
+      },
+    }),
+  ],
   schema: {
-    types: schemaTypes,
+    types: (previousTypes) => {
+      return [...previousTypes, ...schemaTypes]
+    },
   },
 })
--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,6 +1,5 @@
-import { author } from "./author";
-import { blockContent } from "./blockContent";
-import { category } from "./category";
-import { post } from "./post";
+import gallery from './gallery'
+import home from './home'
+import reel from './reel'
 
-export const schemaTypes = [author, blockContent, category, post];
+export const schemaTypes = [home, gallery, reel]
